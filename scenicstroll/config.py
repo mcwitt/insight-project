b4f466ff3b5d8f564e31f51673947d9541f911ed
Matt Wittmann <mcwitt@gmail.com>
2015-10-08 15:32:34 -0700

    Add config and requirements.

---
+++ scenicstroll/config.py
@@ -0,0 +1,9 @@
+DEBUG = True
+DATABASE = 'postgresql://scenic@localhost/scenicstroll2'
+SECRET_KEY = 'secret'
+USERNAME = ''
+PASSWORD = ''
+
+SEARCH_RADIUS = 200
+SIGHT_DISTANCE = 800
+IMAGE_WIDTH = 200
