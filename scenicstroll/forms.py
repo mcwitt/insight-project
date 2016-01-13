1f9dba7f71fe0fd322518271846e7dacb7e37183
Matt Wittmann <mcwitt@gmail.com>
2016-01-13 09:45:07 -0800

    Change default start address.

--- scenicstroll/forms.py
+++ scenicstroll/forms.py
@@ -2,7 +2,7 @@ from wtforms import Form, StringField
 from wtforms.validators import Required
 
 class InputForm(Form):
-    address1 = StringField('address1', [Required()], default='Exploratorium, San Francisco')
+    address1 = StringField('address1', [Required()], default='15 Pier, San Francisco')
     address2 = StringField('address2', [Required()], default='Presidio, San Francisco')
     alpha = StringField('alpha', [Required()], default='5')
 
